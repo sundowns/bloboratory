0ff4f52e39f5c8aaa8cb8ffb9d15a6f53c361b2b
Sundowns <tom@smallridge.com.au>
2018-11-14 23:13:48 +0800

    updated cpath require

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 love.filesystem.setRequirePath(love.filesystem.getRequirePath()..";lib/?.lua;lib/")
-love.filesystem.setCRequirePath(love.filesystem.getRequirePath()..";lib/?.dll;lib/?/?.dll")
+love.filesystem.setCRequirePath(love.filesystem.getCRequirePath()..";lib/?.dll;lib/?/?.dll")
 
 -- Globals (yikes)
 debug = false
