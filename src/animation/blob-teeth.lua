36eed441072478a02c6f9d105e70dfb87ddf5536
Sundowns <tom@smallridge.com.au>
2018-11-23 17:59:04 +0800

    adding boss waves (yeet)

---
+++ src/animation/blob-teeth.lua
@@ -0,0 +1,25 @@
+assert(assets.enemies.blob)
+assert(anim8)
+
+return {
+    id = "BLOB-TEETH",
+    image = assets.enemies.blob,
+    grid = anim8.newGrid(16, 16, assets.enemies.blob:getWidth(), assets.enemies.blob:getHeight()),
+    animation_names = {
+        "DEFAULT"
+    },
+    layers = {
+        {
+            DEFAULT = {
+                frame_duration = 0.1,
+                x = '1-4',
+                y = 6,
+                offset_x = 0,
+                offset_y = 0,
+                scale_x = 2.5,
+                scale_y = 2.5,
+                rotation = 0
+            }
+        }
+    }
+}
