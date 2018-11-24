13d42d374d1abb168b3d62e56e688fb48a767dc3
chappos <40489584+chappos@users.noreply.github.com>
2018-11-24 10:51:29 +0800

    Made laserguns mutable

--- src/tower-stats.lua
+++ src/tower-stats.lua
@@ -34,5 +34,23 @@ return {
             TICK_DURATION = 0.25,
             SPEED_MODIFIER = 0.5, -- 50% total speed
         },
+    },
+    LASERGUN = {
+        FIRE = {
+            DURATION = 2,
+            TICK_DURATION = 0.2,
+            DAMAGE_PER_TICK = 0.4,
+        },
+        ELECTRIC = {
+            DURATION = 3,
+            TICK_DURATION = 0.25,
+            MINIMUM_DAMAGE = 0.5, -- always applied! see below
+            MAXIMUM_EXTRA_DAMAGE = 6, --total maximum is this + minimum
+        },
+        ICE = {
+            DURATION = 1.5,
+            TICK_DURATION = 0.25,
+            SPEED_MODIFIER = 0.5, -- 50% total speed
+        },
     }
 }
