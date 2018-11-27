bfca871c1ca7d1904ba7517963196853c4fa46dc
Sundowns <tom@smallridge.com.au>
2018-11-27 23:36:49 +0800

    added big blob blueprint images

--- src/enemy-blueprints.lua
+++ src/enemy-blueprints.lua
@@ -2,25 +2,25 @@ return {
     ["BLOB-ABSENT"] = EnemyBlueprint("BLOBBY", assets.blueprints['blob'], constants.ENEMY.BLOB.YIELD, true, constants.ENEMY.BLOB.HEALTH, function(params)
         return Blob(params.origin)
     end),
-    ["BLOB-ABSENT-LARGE"] = EnemyBlueprint("BIG BLOBBY", assets.blueprints['blob'], constants.ENEMY.BLOBLARGE.YIELD, false, constants.ENEMY.BLOBLARGE.HEALTH, function(params)
+    ["BLOB-ABSENT-LARGE"] = EnemyBlueprint("BIG BLOBBY", assets.blueprints['blob-large'], constants.ENEMY.BLOBLARGE.YIELD, false, constants.ENEMY.BLOBLARGE.HEALTH, function(params)
         return BlobLarge(params.origin)
     end),
     ["BLOB-FIRE"] = EnemyBlueprint("FLAREY", assets.blueprints['blob-fire'], constants.ENEMY.BLOBFIRE.YIELD, true, constants.ENEMY.BLOBFIRE.HEALTH, function(params)
         return BlobFire(params.origin)
     end),
-    ["BLOB-FIRE-LARGE"] = EnemyBlueprint("BIG FLAREY", assets.blueprints['blob-fire'], constants.ENEMY.BLOBFIRELARGE.YIELD, false, constants.ENEMY.BLOBFIRELARGE.HEALTH, function(params)
+    ["BLOB-FIRE-LARGE"] = EnemyBlueprint("BIG FLAREY", assets.blueprints['blob-fire-large'], constants.ENEMY.BLOBFIRELARGE.YIELD, false, constants.ENEMY.BLOBFIRELARGE.HEALTH, function(params)
         return BlobFireLarge(params.origin)
     end),
     ["BLOB-ICE"] = EnemyBlueprint("ICEY", assets.blueprints['blob-ice'], constants.ENEMY.BLOBICE.YIELD, true, constants.ENEMY.BLOBICE.HEALTH, function(params)
         return BlobIce(params.origin)
     end),
-    ["BLOB-ICE-LARGE"] = EnemyBlueprint("BIG ICEY", assets.blueprints['blob-ice'], constants.ENEMY.BLOBICELARGE.YIELD, false, constants.ENEMY.BLOBICELARGE.HEALTH, function(params)
+    ["BLOB-ICE-LARGE"] = EnemyBlueprint("BIG ICEY", assets.blueprints['blob-ice-large'], constants.ENEMY.BLOBICELARGE.YIELD, false, constants.ENEMY.BLOBICELARGE.HEALTH, function(params)
         return BlobIceLarge(params.origin)
     end),
     ["BLOB-SPARK"] = EnemyBlueprint("SPARKY", assets.blueprints['blob-electric'], constants.ENEMY.BLOBSPARK.YIELD, true, constants.ENEMY.BLOBSPARK.HEALTH, function(params)
         return BlobSpark(params.origin)
     end),
-    ["BLOB-SPARK-LARGE"] = EnemyBlueprint("BIG SPARKY", assets.blueprints['blob-electric'], constants.ENEMY.BLOBSPARKLARGE.YIELD, false, constants.ENEMY.BLOBSPARKLARGE.HEALTH, function(params)
+    ["BLOB-SPARK-LARGE"] = EnemyBlueprint("BIG SPARKY", assets.blueprints['blob-electric-large'], constants.ENEMY.BLOBSPARKLARGE.YIELD, false, constants.ENEMY.BLOBSPARKLARGE.HEALTH, function(params)
         return BlobSparkLarge(params.origin)
     end),
     ["BLOB-SKULL"] = EnemyBlueprint("BLOB (SKULL)", assets.blueprints['blob-skull'], constants.ENEMY.BLOBSKULL.YIELD, false, constants.ENEMY.BLOBSKULL.HEALTH, function(params)
