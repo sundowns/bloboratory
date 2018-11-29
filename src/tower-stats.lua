29d5bbfa490c70c94388dd6598f1f35602b372eb
chappos <40489584+chappos@users.noreply.github.com>
2018-11-29 22:01:24 +0800

    Nerfed bouncer upgrades

--- src/tower-stats.lua
+++ src/tower-stats.lua
@@ -75,16 +75,16 @@ return {
         FIRE = {
             DURATION = 2,
             TICK_DURATION = 0.25,
-            DAMAGE_PER_TICK = 2,
+            DAMAGE_PER_TICK = 1,
         }, 
         ELECTRIC = {
             DURATION = 2,
             TICK_DURATION = 0.25,
             MINIMUM_DAMAGE = 0.1, -- always applied! see below
-            MAXIMUM_EXTRA_DAMAGE = 4, --total maximum is this + minimum
+            MAXIMUM_EXTRA_DAMAGE = 2, --total maximum is this + minimum
         }, 
         ICE = {
-            DURATION = 2,
+            DURATION = 1,
             TICK_DURATION = 0.25,
             SPEED_MODIFIER = 0.5, -- 50% total speed
         },
