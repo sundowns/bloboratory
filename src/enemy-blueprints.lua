fb3762026e6fd7767c199af0d9231dd165c8d0cc
chappos <40489584+chappos@users.noreply.github.com>
2018-11-29 23:49:10 +0800

    Gave players access to chompers and numbskulls on round 26

--- src/enemy-blueprints.lua
+++ src/enemy-blueprints.lua
@@ -26,13 +26,13 @@ return {
     ["BLOB-SKULL"] = EnemyBlueprint("BONEHEAD", assets.blueprints['blob-skull'], constants.ENEMY.BLOBSKULL.YIELD, false, {21, 31}, constants.ENEMY.BLOBSKULL.HEALTH, function(params)
         return BlobSkull(params.origin)
     end),
-    ["BLOB-SKULL-DARK"] = EnemyBlueprint("NUMBSKULL", assets.blueprints['blob-skull-dark'], constants.ENEMY.BLOBSKULL_DARK.YIELD, false, {31, 31}, constants.ENEMY.BLOBSKULL_DARK.HEALTH, function(params)
+    ["BLOB-SKULL-DARK"] = EnemyBlueprint("NUMBSKULL", assets.blueprints['blob-skull-dark'], constants.ENEMY.BLOBSKULL_DARK.YIELD, false, {26, 31}, constants.ENEMY.BLOBSKULL_DARK.HEALTH, function(params)
         return BlobSkullDark(params.origin)
     end),
     ["BLOB-TEETH"] = EnemyBlueprint("NIBBLER", assets.blueprints['blob-teeth'], constants.ENEMY.BLOBTEETH.YIELD, false, {21, 31}, constants.ENEMY.BLOBTEETH.HEALTH, function(params)
         return BlobTeeth(params.origin)
     end),
-    ["BLOB-TEETH-DARK"] = EnemyBlueprint("CHOMPER", assets.blueprints['blob-teeth-dark'], constants.ENEMY.BLOBTEETH_DARK.YIELD, false, {31, 31}, constants.ENEMY.BLOBTEETH_DARK.HEALTH, function(params)
+    ["BLOB-TEETH-DARK"] = EnemyBlueprint("CHOMPER", assets.blueprints['blob-teeth-dark'], constants.ENEMY.BLOBTEETH_DARK.YIELD, false, {26, 31}, constants.ENEMY.BLOBTEETH_DARK.HEALTH, function(params)
         return BlobTeethDark(params.origin)
     end),
     ["BLOB-EYE"] = EnemyBlueprint("SEER", assets.blueprints['blob-eye'], constants.ENEMY.BLOBEYE.YIELD, false, {31, 31}, constants.ENEMY.BLOBEYE.HEALTH, function(params)
