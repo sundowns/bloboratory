4f7dadb715e7a6f60db5d01485d109866afd4e2a
Sundowns <tom@smallridge.com.au>
2018-11-30 19:45:35 +0800

    add logo

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ function love.load()
     GamestateManager = require("lib.gamestate")
     require("loading")
     require("game")
-
+    love.window.setIcon(love.image.newImageData('asset/blueprints/beacon.png'))
     GamestateManager.registerEvents()
     GamestateManager.switch(loading)
 end
